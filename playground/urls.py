cf38a0edd6f4732885f20ee7b9c207056f265c86
Ankus Nayak <iamankus7@gmail.com>
2025-06-18 18:34:14 +0530

    Register the url for playground app's say_hello views

--- playground/urls.py
+++ playground/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path  # From django.urls import path for defining the path.
-from . import views  # From current folder import views module so that we can reference our view functions
-
+# Import path funtions from django.urls
+from django.urls import path
+# from current folder import views module
+from . import views
 
 """ In urls module we gonna map urls to views or view function.
 """
+# URLConf
+urlpatterns = [
+    # arrays of urlpatterns objects.
+    # use path function for creating url
+
+    # always ends routes with forward slash
+    path('hello/', views.say_hello)
+]
